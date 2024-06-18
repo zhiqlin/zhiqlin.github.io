e775afc66132764c81daa87a0e154db30ecb15e9
Zhiqiang Lin <zlin@cse.ohio-state.edu>
2024-06-18 10:46:47 -0400

    New website

---
+++ email.js
@@ -0,0 +1,4 @@
+<script type="text/javascript">dont_want_spam=(':zlin@' + "cse.o" + "hio-state" + ".e" + "du")
+  subtle=dont_want_spam.substring(1,26);
+  document.write('<a href="mailto' + dont_want_spam + '">' + subtle + '</a>')
+</script>
---
+++ iff.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZHW668HTFG"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZHW668HTFG');
+</script>
